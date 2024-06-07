94a8ebe0c045d7ca30b0d415d1f32b3647b4d633
Muvels <maroltmatteo@gmail.com>
2024-06-08 00:42:20 +0200

    feat: add documents as possible upload

--- components/Editor/types.ts
+++ components/Editor/types.ts
@@ -6,4 +6,4 @@ export interface BlockNote {
     children: BlockNote[]
 }
 
-export const UploadTypes = ["image", "database", "video", "audio"];
+export const UploadTypes = ["image", "database", "video", "audio", "file"];
